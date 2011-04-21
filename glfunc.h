ee701fbc33864a84f9cedaf65750fa8a5238dc3a
psyomn <psyomn@xenagoras.net>
2011-04-21 03:57:40 -0400

    Woops. forgot the glut functions.

---
+++ glfunc.h
@@ -0,0 +1,7 @@
+void setup(){
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glRotatef(27.0, 1.0, 0.0, 0.0);
+	glRotatef(27.0, 0.0, 1.0, 0.0);
+	glMatrixMode(GL_MODELVIEW);
+}
